842e7dbe2eeeecd09dd58b78f96cb08fa2e32f58
Benladamm <yyy618442@gmail.com>
2025-12-11 16:25:05 +0100

    fix: Corriger l'identifiant de l'application pour le client SSH moderne

--- src/main.c
+++ src/main.c
@@ -24,7 +24,7 @@ int main(int argc, char **argv) {
     GtkApplication *app;
     int status;
 
-    app = gtk_application_new("com.benladamm.modernssh", G_APPLICATION_DEFAULT_FLAGS);
+    app = gtk_application_new("io.github.benladamm.ssh_client", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     status = g_application_run(G_APPLICATION(app), argc, argv);
     
